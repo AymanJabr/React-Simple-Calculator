1b0f588dcf4369eab1f4f3e43a7755f50e5f3c38
ayman <aymanjaber2012@gmail.com>
2021-05-28 11:46:43 +0300

    Add React class Components

---
+++ src/components/ButtonPanel.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+class ButtonPanel extends React.Component {
+
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default ButtonPanel
