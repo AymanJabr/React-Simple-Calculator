f1589e4180bf08704e5521122a1e780d6b82711d
ayman <aymanjaber2012@gmail.com>
2021-05-31 08:22:30 +0300

    Refactor components

--- src/components/App.js
+++ src/components/App.js
@@ -1,33 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ButtonPanel from './ButtonPanel';
 import Display from './Display';
 
 import calculate from '../logic/calculate';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { total: null, operation: null, next: null };
+function App() {
+  const [total, setTotal] = useState(null);
+  const [next, setNext] = useState(null);
+  const [operation, setOperation] = useState(null);
 
-    this.handleClick = this.handleClick.bind(this);
-  }
+  const handleClick = (buttonName) => {
+    const {
+      next: newNext,
+      operation: newOperation,
+      total: newTotal,
+    } = calculate({ next, operation, total }, buttonName);
 
-  handleClick = (buttonName) => {
-    const { next, total, operation } = calculate(this.state, buttonName);
+    setTotal(newTotal);
+    setNext(newNext);
+    setOperation(newOperation);
+  };
 
-    this.setState({ total, next, operation });
-  }
-
-  render() {
-    const { total, next, operation } = this.state;
-
-    return (
-      <>
-        <Display total={total} next={next} operation={operation} />
-        <ButtonPanel clickHandler={this.handleClick} />
-      </>
-    );
-  }
+  return (
+    <>
+      <Display total={total} next={next} operation={operation} />
+      <ButtonPanel clickHandler={handleClick} />
+    </>
+  );
 }
 
 export default App;
