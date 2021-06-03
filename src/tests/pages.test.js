e8c6c0b32c03b7bae5fdc5a8c8e0b21c9110aec1
ayman <aymanjaber2012@gmail.com>
2021-06-03 19:35:25 +0300

    Add jest snapshots

---
+++ src/tests/pages.test.js
@@ -0,0 +1,26 @@
+import Home from '../pages/Home'
+import React from 'react'
+import renderer from 'react-test-renderer'
+import { BrowserRouter } from 'react-router-dom';
+import Quote from '../pages/Quote';
+import Calculator from '../pages/Calculator';
+
+
+
+it('Correctly renders the Home page', () => {
+    const tree = renderer.create(<BrowserRouter> <Home />
+        </BrowserRouter>).toJSON();
+    expect(tree).toMatchSnapshot();
+});
+
+it('Correctly renders the Quote page', () => {
+    const tree = renderer.create(<BrowserRouter> <Quote />
+    </BrowserRouter>).toJSON();
+    expect(tree).toMatchSnapshot();
+});
+
+it('Correctly renders the Calculator page', () => {
+    const tree = renderer.create(<BrowserRouter> <Calculator />
+    </BrowserRouter>).toJSON();
+    expect(tree).toMatchSnapshot();
+});
